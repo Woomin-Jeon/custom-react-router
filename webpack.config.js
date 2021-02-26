706ec6d4e75589ca5322fc47d00a112a059fefdb
woomin <dal96k@gmail.com>
2021-02-26 14:38:25 +0900

    [Init] 초기 개발환경 설정

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+};
