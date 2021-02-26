66e220c8103e1110ddecf9f2fbe9f5935ec69442
woomin <dal96k@gmail.com>
2021-02-26 14:42:46 +0900

    [Feat] 초기 페이지 및 네이게이터 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div>
-      hello world!
-    </div>
+    <>
+      <Navigator />
+      <Home />
+      <Login />
+      <About />
+    </>
   );
+}
+
+function Navigator() {
+  return (
+    <>
+      <button type="button">/home</button>
+      <button type="button">/login</button>
+      <button type="button">/about</button>
+    </>
+  );
+}
+
+function Home() {
+  return <h1>Home 페이지 입니다</h1>;
+}
+
+function Login() {
+  return <h1>Login 페이지 입니다</h1>;
+}
+
+function About() {
+  return <h1>About 페이지 입니다</h1>;
 }
